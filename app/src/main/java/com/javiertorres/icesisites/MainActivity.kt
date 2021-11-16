4afe64ab528a7b738c1e92fd7a5f51521c5819bb
Javier Torres <javierandrestorresreyes@gmail.com>
2021-11-15 21:39:02 -0500

    basic navigation layout

--- app/src/main/java/com/javiertorres/icesisites/MainActivity.kt
+++ app/src/main/java/com/javiertorres/icesisites/MainActivity.kt
@@ -2,10 +2,17 @@ package com.javiertorres.icesisites
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.splash)
+        val timer = object: CountDownTimer(2000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {}
+
+            override fun onFinish() {setContentView(R.layout.activity_main)}
+        }
+        timer.start()
     }
 }
